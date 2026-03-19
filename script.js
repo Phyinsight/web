b7e727a24af88725a20e43529640c2b0cc98dc82
Phyinsight <phyinsight.team@gmail.com>
2026-03-19 12:53:31 +0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const menuBtn = document.getElementById("menuBtn")
-const nav = document.querySelector("nav")
-
-if(menuBtn){
-menuBtn.onclick = () => {
-nav.style.display = nav.style.display === "flex" ? "none" : "flex"
-}
-}
